0ddb651c24f3f99299ab394fe1666eabba6c0875
daniel <daniele-garzons@unilibre.edu.co>
2025-10-15 22:50:42 -0500

    Update CineMatch.py

--- CineMatch.py
+++ CineMatch.py
@@ -37,6 +37,7 @@ def get_recommendations(title):
         return []
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)[1:6]
+    
         
     
 
